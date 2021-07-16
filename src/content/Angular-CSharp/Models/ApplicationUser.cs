65d7a026c6566afe4af7463648bba5e06db925da
Damian Edwards <damian@damianedwards.com>
2021-07-15 18:29:27 -0700

    Update SPA templates for C#10 (#1)
    - Implicit usings
    - File-scoped namespace declarations

--- src/content/Angular-CSharp/Models/ApplicationUser.cs
+++ src/content/Angular-CSharp/Models/ApplicationUser.cs
@@ -1,12 +1,8 @@
 ﻿using Microsoft.AspNetCore.Identity;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
-namespace Company.WebApplication1.Models
+namespace Company.WebApplication1.Models;
+
+public class ApplicationUser : IdentityUser
 {
-    public class ApplicationUser : IdentityUser
-    {
-    }
+    
 }
