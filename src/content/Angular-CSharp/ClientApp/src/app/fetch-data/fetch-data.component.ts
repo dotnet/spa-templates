5628b252c2e8ca412231329f7179711866297580
Stephen Halter <halter73@gmail.com>
2023-05-17 17:42:36 -0700

    Remove IdentityServer from angular template
    - Use default Identity UI Razor Pages with cookies instead

--- src/content/Angular-CSharp/ClientApp/src/app/fetch-data/fetch-data.component.ts
+++ src/content/Angular-CSharp/ClientApp/src/app/fetch-data/fetch-data.component.ts
@@ -1,5 +1,4 @@
 import { Component, Inject } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-fetch-data',
@@ -8,8 +7,15 @@ import { HttpClient } from '@angular/common/http';
 export class FetchDataComponent {
   public forecasts: WeatherForecast[] = [];
 
-  constructor(http: HttpClient, @Inject('BASE_URL') baseUrl: string) {
-    http.get<WeatherForecast[]>(baseUrl + 'weatherforecast').subscribe(result => {
+  constructor(@Inject('BASE_URL') baseUrl: string) {
+    fetch(baseUrl + 'weatherforecast').then(response => {
+////#if (IndividualLocalAuth)
+      if (response.redirected && response.url.startsWith(`${baseUrl}Identity/Account/Login`)) {
+        window.location.href = `${baseUrl}Identity/Account/Login?ReturnUrl=/fetch-data`;
+      }
+////#endif
+      return response.json();
+    }).then(result => {
       this.forecasts = result;
     }, error => console.error(error));
   }
