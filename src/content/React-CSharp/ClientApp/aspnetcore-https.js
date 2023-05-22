2e63a8c808017073457df8a5cd3e001ce84d7d87
Shiladitya Mukerji <shiladitya.mukerji@gmail.com>
2023-05-22 17:27:10 +0530

    Fixed Certificate rotation issue and added validity check

--- src/content/React-CSharp/ClientApp/aspnetcore-https.js
+++ src/content/React-CSharp/ClientApp/aspnetcore-https.js
@@ -1,35 +1,34 @@
 // This script sets up HTTPS for the application using the ASP.NET Core HTTPS certificate
 const fs = require('fs');
-const spawn = require('child_process').spawn;
+const { spawnSync } = require('child_process');
 const path = require('path');
 
 const baseFolder =
-  process.env.APPDATA !== undefined && process.env.APPDATA !== ''
-    ? `${process.env.APPDATA}/ASP.NET/https`
-    : `${process.env.HOME}/.aspnet/https`;
+    process.env.APPDATA !== undefined && process.env.APPDATA !== ''
+        ? `${process.env.APPDATA}/ASP.NET/https`
+        : `${process.env.HOME}/.aspnet/https`;
 
 const certificateArg = process.argv.map(arg => arg.match(/--name=(?<value>.+)/i)).filter(Boolean)[0];
 const certificateName = certificateArg ? certificateArg.groups.value : process.env.npm_package_name;
 
 if (!certificateName) {
-  console.error('Invalid certificate name. Run this script in the context of an npm/yarn script or pass --name=<<app>> explicitly.')
-  process.exit(-1);
+    console.error('Invalid certificate name. Run this script in the context of an npm/yarn script or pass --name=<<app>> explicitly.')
+    process.exit(-1);
 }
 
 const certFilePath = path.join(baseFolder, `${certificateName}.pem`);
 const keyFilePath = path.join(baseFolder, `${certificateName}.key`);
 
-if (!fs.existsSync(certFilePath) || !fs.existsSync(keyFilePath)) {
-  spawn('dotnet', [
-    'dev-certs',
-    'https',
-    '--check',
-    '--export-path',
-    certFilePath,
-    '--format',
-    'Pem',
-    '--no-password',
-    '--trust',
-  ], { stdio: 'inherit', })
-  .on('exit', (code) => process.exit(code));
+const validationResult = spawnSync('dotnet', ['dev-certs', 'https', '--check'], { encoding: 'utf8' });
+
+if (validationResult.status !== 0) {
+    spawnSync('dotnet', [
+        'dev-certs',
+        'https',
+        '--export-path',
+        certFilePath,
+        '--format',
+        'Pem',
+        '--no-password',
+    ], { stdio: 'inherit' });
 }
